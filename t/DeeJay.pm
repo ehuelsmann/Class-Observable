0318645f7e0678a503e6a007ea8abf800d396668
Chris Winters <chris@cwinters.com>
2004-10-16 16:48:50 +0000

    add weak references to coderef/object observers; add observer_log() and observer_error()

--- t/DeeJay.pm
+++ t/DeeJay.pm
@@ -5,29 +5,33 @@ package DeeJay;
 use strict;
 
 sub new {
-    my ( $class, $playlist ) = @_;
+    my ( $class, $playlist, $log ) = @_;
     $playlist ||= [];
-    my $self = bless( {}, $class );
-    $self->{playlist} = $playlist;
-    $self->{num_songs} = scalar @{ $playlist };
-    return $self;
+    return bless( {
+        playlist  => $playlist,
+        num_songs => scalar @{ $playlist },
+        $log      => $log,
+    }, $class );
 }
 
 sub start_party {
     my ( $self ) = @_;
-    #print "Let's get this party started!\n";
+    $self->{log} &&
+        $self->{log}->( "Let's get this party started!" );
     $self->{current_song} = 0;
     $self->{playlist}[0]->play;
 }
 
 sub end_party {
     my ( $self ) = @_;
-    #print "Party's over, time to go home\n";
+    $self->{log} &&
+        $self->{log}->( "Party's over, time to go home" );
 }
 
 sub update {
     my ( $self, $song, $action ) = @_;
-    #print "Caught update [$action] from [$song->{band}]\n";
+    $self->{log} &&
+        $self->{log}->( "Caught update [$action] from [$song->{band}]" );
     $self->{update}++;
     return unless ( $action eq 'stop_play' );
     $self->{update_stop}++;
@@ -41,6 +45,14 @@ sub update {
 sub num_updates      { return $_[0]->{update} }
 sub num_updates_stop { return $_[0]->{update_stop} }
 
+sub DESTROY {
+    my ( $self ) = @_;
+    $self->{log} &&
+        $self->{log}->( "DeeJay retiring" );
+}
+
+1;
+
 package DeeJay::Selfish;
 
 # This DJ only responds to his/her own songs
@@ -48,16 +60,20 @@ package DeeJay::Selfish;
 use strict;
 
 sub new {
-    my ( $class, $my_name ) = @_;
-    return bless( { name        => $my_name,
-                    update      => 0,
-                    update_self => 0 }, $class );
+    my ( $class, $my_name, $log ) = @_;
+    return bless({
+        name        => $my_name,
+        update      => 0,
+        update_self => 0,
+        log         => $log,
+    }, $class );
 }
 
 sub update {
     my ( $self, $song ) = @_;
     $self->{update}++;
-    #print "I am [$self->{name}] song is [$song->{band}]\n";
+    $self->{log} &&
+        $self->{log}->( "I am '$self->{name}' song is '$song->{band}'" );
     $self->{update_self}++ if ( $song->{band} eq $self->{name} );
 }
 
@@ -68,7 +84,12 @@ package DeeJay::Helper;
 
 use strict;
 
-sub new { return bless( {}, $_[0] ) }
+sub new {
+    my ( $class, $log ) = @_;
+    return bless({
+        log => $log,
+    }, $class );
+}
 
 sub update {
     my ( $self, $song ) = @_;
--- t/Song.pm
+++ t/Song.pm
@@ -6,24 +6,33 @@ use strict;
 use base qw( Class::Observable );
 
 sub new {
-    my ( $class, $band, $name ) = @_;
-    my $self = bless( {}, $class );
-    $self->{band} = $band;
-    $self->{name} = $name;
-    return $self;
+    my ( $class, $band, $name, $log ) = @_;
+    return bless( {
+        band => $band,
+        name => $name,
+        log  => $log,
+    }, $class );
 }
 
 sub play {
     my ( $self ) = @_;
     $self->notify_observers( 'start_play' );
-    #print "Playing [$self->{name}] by [$self->{band}]\n";
+    $self->{log} &&
+        $self->{log}->( "Playing [$self->{name}] by [$self->{band}]" );
     $self->stop;
 }
 
 sub stop {
     my ( $self ) = @_;
-    #print "Stopped [$self->{name}] by [$self->{band}]\n";
+    $self->{log} &&
+        $self->{log}->( "Stopped [$self->{name}] by [$self->{band}]" );
     $self->notify_observers( 'stop_play' );
+}
+
+sub DESTROY {
+    my ( $self ) = @_;
+    $self->{log} &&
+        $self->{log}->( "Destroying '$self->{name}'" );
 }
 
 1;
